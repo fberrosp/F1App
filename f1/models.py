7e8c40fd4227ea2c93085b6a4e63506d3475bc21
Fernando Berrospi <fberrosp@gmail.com>
2022-12-30 19:40:42 -0500

    saved drivers table to DB successfuly

--- f1/models.py
+++ f1/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.db.models.fields import UUIDField, IntegerField, CharField, DateField, FloatField, TimeField
 
+
 class Drivers(models.Model):
-    driverId = UUIDField(primary_key=True, editable=False)
-    driverRef = CharField(max_length=255)
-    number = IntegerField(max_length=11)
-    code = CharField(max_length=3)
+    driverId = CharField(max_length=255, primary_key=True, editable=False)
+    number = IntegerField(blank=True, null=True)
+    code = CharField(max_length=3, blank=True, null=True)
     forename = CharField(max_length=255)
     surname = CharField(max_length=255)
     dob = DateField()
--- f1/services.py
+++ f1/services.py
@@ -4,36 +4,46 @@ from .models import Drivers
 from datetime import date
 
 # method to get F1 API data from Ergast API
+
+
 def get_api_data(api_endpoint):
     limitedUrl = 'https://ergast.com/api/f1/{}.json'.format(api_endpoint)
     response = requests.get(limitedUrl).json()
     total = response['MRData']['total']
-    unlimitedUrl = 'https://ergast.com/api/f1/{}?limit={}.json'.format(api_endpoint, total)
-    #unlimitedResponse = requests.get(unlimitedUrl).json()
+    unlimitedUrl = 'https://ergast.com/api/f1/{}.json?limit={}'.format(
+        api_endpoint, total)
+    unlimitedResponse = requests.get(unlimitedUrl).json()
 
-    return response['MRData']
+    return unlimitedResponse['MRData']
+
 
 def get_drivers():
     drivers = get_api_data('drivers')['DriverTable']['Drivers']
-    print(drivers)
-    
+
     for driver in drivers:
-        print(driver)
-        '''
+
+        try:
+            driverNumber = driver['permanentNumber']
+        except:
+            driverNumber = None
+
+        try:
+            driverCode = driver['code']
+        except:
+            driverCode = None
+
         driver_data = Drivers(
-            driverId = driver['driverId'],
-            driverRef = driver['driverRef'],
-            number = driver['permanentNumber'],
-            code = driver['code'],
-            forename = driver['givenName'],
-            surename = driver['familyName'],
-            dob = driver['dateOfBirth'],
-            nationality = driver['nationality'],
-            url = driver['url']
+            driverId=driver['driverId'],
+            number=driverNumber,
+            code=driverCode,
+            forename=driver['givenName'],
+            surname=driver['familyName'],
+            dob=driver['dateOfBirth'],
+            nationality=driver['nationality'],
+            url=driver['url']
         )
-        '''
-        #driver_data.save()
-        #all_drivers = Drivers.objects.all().order_by('driverId')
-        all_drivers = drivers
+
+        driver_data.save()
+        all_drivers = Drivers.objects.all().order_by('driverId')
 
     return all_drivers
