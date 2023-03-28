26ad2e674f68a406995a6e1a294c948d34c20e20
Fernando Berrospi <fberrosp@gmail.com>
2023-03-28 16:47:23 -0500

    implemented view caching using redis

--- f1/views.py
+++ f1/views.py
@@ -1,49 +1,77 @@
 from django.conf import settings
+from django.core.cache import cache
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
 from django.shortcuts import render, redirect
 from django.views.decorators.cache import cache_page
+
 from .services import *
-from datetime import date
+
+from functools import wraps
 
 CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
-def get_season(request):
-    season = request.session.get('season', int(date.today().year))
-    selected_season = request.GET.get('selected_season')
+#caches the entire view
+def cache_page_redis(timeout):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(request, *args, **kwargs):
+            season = request.session.get('season', int(date.today().year))
+            cache_key = f"{func.__name__}_{season}"
+            response = cache.get(cache_key)
+            if response is None:
+                response = func(request, *args, **kwargs)
+                cache.set(cache_key, response, timeout)
+            return response
+        return wrapper
+    return decorator
 
-    if selected_season and selected_season != season:
-        request.session['season'] = int(selected_season)
-        season = selected_season
-
-    return season
-
+@cache_page_redis(CACHE_TTL)
 def index(request):
-    season = get_season(request)
-
+    season = request.session.get('season', int(date.today().year))
     return render(request, 'index.html', {'season': season})
 
-#@cache_page(CACHE_TTL)
+@cache_page_redis(CACHE_TTL)
 def drivers(request):
-    season = get_season(request)
-    drivers = get_drivers(request)
+    season = request.session.get('season', int(date.today().year))
+    drivers = cache.get(f"drivers_{season}")
+
+    if drivers is None:
+        drivers = get_drivers(request)
+        cache.set(f"drivers_{season}", drivers, CACHE_TTL)
+
     return render(request, 'drivers.html', {'drivers': drivers, 'season': season})
 
-#@cache_page(CACHE_TTL)
+@cache_page_redis(CACHE_TTL)
 def constructors(request):
-    season = get_season(request)
-    constructors = get_constructors(request)
+    season = request.session.get('season', int(date.today().year))
+    constructors = cache.get(f"constructors_{season}")
+
+    if constructors is None:
+        constructors = get_constructors(request)
+        cache.set(f"constructors_{season}", constructors, CACHE_TTL)
+
     return render(request, 'constructors.html', {'constructors': constructors, 'season': season})
 
-#@cache_page(CACHE_TTL)
+@cache_page_redis(CACHE_TTL)
 def circuits(request):
-    season = get_season(request)
-    circuits = get_circuits(request)
+    season = request.session.get('season', int(date.today().year))
+    circuits = cache.get(f"circuits_{season}")
+
+    if circuits is None:
+        circuits = get_circuits(request)
+        cache.set(f"circuits_{season}", circuits, CACHE_TTL)
+
     return render(request, 'circuits.html', {'circuits': circuits, 'season': season})
 
-#@cache_page(CACHE_TTL)
+@cache_page_redis(CACHE_TTL)
 def races(request):
-    season = get_season(request)
-    races = get_races(request)
+    season = request.session.get('season', int(date.today().year))
+    races = cache.get(f"races_{season}")
+
+    if races is None:
+        races = get_races(request)
+        cache.set(f"races_{season}", races, CACHE_TTL)
+
     return render(request, 'races.html', {'races': races, 'season': season})
 
 #@cache_page(CACHE_TTL)
