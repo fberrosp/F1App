a4f0ccc90f16aaa68b1c7f0821c77cd0fb2dd615
Fernando Berrospi <fberrosp@gmail.com>
2023-03-28 16:47:08 -0500

    added cache custom middleware

--- formula1WebApp/settings.py
+++ formula1WebApp/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'f1.middleware.SeasonMiddleware',
 ]
 
 ROOT_URLCONF = 'formula1WebApp.urls'
